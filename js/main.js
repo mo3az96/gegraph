7c24d38cb73494faef96f56578e97899d7caee80
mo3az96 <mo3az.abdellatif@gmail.com>
2024-03-02 09:59:00 +0200

    work animate

--- js/main.js
+++ js/main.js
@@ -274,4 +274,27 @@ $(document).ready(function () {
     }
     $(this).next().html(file_val);
   });
+
+  if ($(window).width() >= 1192 && $(".home-works").length) {
+    var images = gsap.utils.toArray(".home-works .work-item:last-child .work-content");
+
+    images.forEach((image, i) => {
+      gsap.fromTo(
+        image,
+        { scale: 0.78947 },
+        {
+          scale: 1,
+          ease: "none",
+          scrollTrigger: {
+            pin: ".works-list",
+            trigger: ".work-item:last-child",
+            start: "top top",
+            end: "bottom top",
+            scrub: 0.1,
+            markers: true,
+          },
+        }
+      );
+    });
+  }
 });
